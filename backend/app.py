91af3528875b78a90678754ad26e1299a24d4174
Shivanichn6 <shivanichn26@gmail.com>
2026-05-20 20:16:11 +0530

    spacing fix

--- backend/app.py
+++ backend/app.py
@@ -623,9 +623,9 @@ def admin_dashboard():
 
 if __name__ == "__main__":
     init_db()
-    
+
     import os
-    
+
     app.run(
         host="0.0.0.0",
         port=int(os.environ.get("PORT", 5000)),
