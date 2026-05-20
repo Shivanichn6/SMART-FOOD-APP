7c0e63e595e04227351b1349f5e54ea75434e891
Shivanichn6 <shivanichn26@gmail.com>
2026-05-20 19:08:30 +0530

    port fixed

--- backend/app.py
+++ backend/app.py
@@ -625,4 +625,10 @@ if __name__ == '__main__':
 
     init_db()
 
-   app.run(host="0.0.0.0", port=5000, debug=True)
+   import os
+
+app.run(
+    host="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),
+    debug=True
+)
